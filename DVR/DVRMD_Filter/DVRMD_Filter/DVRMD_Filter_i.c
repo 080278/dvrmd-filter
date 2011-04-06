02d8569edbc7a80ef979a447de4954d9ae4b3937
shengang1978@gmail.com <shengang1978@gmail.com@27769579-7047-b306-4d6f-d36f87483bb3>
2011-04-06 01:09:46 +0000

    Frame work of the MetaData

--- DVR/DVRMD_Filter/DVRMD_Filter/DVRMD_Filter_i.c
+++ DVR/DVRMD_Filter/DVRMD_Filter/DVRMD_Filter_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sat Apr 02 21:04:35 2011
+/* at Mon Apr 04 23:44:09 2011
  */
 /* Compiler settings for DVRMD_Filter.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
